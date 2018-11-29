a646cd61e7836b1b34360265fcc10516188372d9
Florian Ludot <florian.ludot@gmail.com>
2018-11-29 13:44:03 +0900

    Turi Create python script updated

--- Turi Create Resources/WallyML.py
+++ Turi Create Resources/WallyML.py
@@ -1,41 +1,23 @@
 import turicreate as tc
 
-training_images = tc.load_images('images/')
-print(training_images)
+images = tc.load_images('images/')
+print(images)
 
-training_annotations = tc.SFrame('annotations.csv')
-print(training_annotations)
+annotations = tc.SFrame('annotations.csv')
+print(annotations)
 
-training_data = training_images.join(training_annotations)
+data = images.join(annotations)
+print(data)
+
+training_data, testing_data = data.random_split(0.95)
 print(training_data)
-
-testing_images = tc.load_images('eval_images/')
-print(testing_images)
-
-testing_annotations = tc.SFrame('annotations_evaluate.csv')
-print(testing_annotations)
-
-testing_data = testing_images.join(testing_annotations)
 print(testing_data)
 
-model = tc.object_detector.create(training_data, max_iterations = 10000)
+model = tc.object_detector.create(training_data, max_iterations=5000)
 
-model.save('model/annotationsgithu2.model')
-
-predictions = model.predict(testing_data)
-print(predictions[0])
-print(predictions[1])
-print(predictions[2])
-print(predictions[4])
-
-testing_data['predicted_image'] = tc.object_detector.util.draw_bounding_boxes(testing_data['image'], predictions)
-
-testing_data['predicted_image'][0].show()
-testing_data['predicted_image'][1].show()
-testing_data['predicted_image'][2].show()
-testing_data['predicted_image'][4].show()
+model.save('model/WallyML.model')
 
 metric = model.evaluate(testing_data)
 print(metric)
 
-model.export_coreml('model/annotationsgithu.mlmodel', include_non_maximum_suppression=False)
+model.export_coreml('model/WallyML.mlmodel', include_non_maximum_suppression=False)
