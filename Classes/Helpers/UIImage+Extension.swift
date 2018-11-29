95924d06621027240e14107f1cf22183939059d5
Florian Ludot <florian.ludot@gmail.com>
2018-11-29 13:33:21 +0900

    uiimage scale extension

--- Classes/Helpers/UIImage+Extension.swift
+++ Classes/Helpers/UIImage+Extension.swift
@@ -42,3 +42,23 @@ func imagesFromFolder(_ folder: String) -> [UIImage] {
     fatalError("Bundle.main.resourcePath")
     
 }
+
+extension UIImage {
+    
+    func scaleImage(toSize newSize: CGSize) -> UIImage? {
+        var newImage: UIImage?
+        let newRect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        if let context = UIGraphicsGetCurrentContext(), let cgImage = self.cgImage {
+            context.interpolationQuality = .high
+            let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: newSize.height)
+            context.concatenate(flipVertical)
+            context.draw(cgImage, in: newRect)
+            if let img = context.makeImage() {
+                newImage = UIImage(cgImage: img)
+            }
+            UIGraphicsEndImageContext()
+        }
+        return newImage
+    }
+}
